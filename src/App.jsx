c4c3410ff8d9e5bcb187bd3854358384ce40b08f
omarjafor <omarjaforchy@gmail.com>
2023-09-13 22:00:33 +0600

    prepeared

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
 
@@ -6,7 +7,9 @@ function App() {
     <>
       <h1 className="text-3xl font-bold">
         Welcome To Avengers
+        
       </h1>
+      <Home></Home>
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h2>Hellooooooooooooo</h2>
+        </div>
+    );
+};
+
+export default Home;
