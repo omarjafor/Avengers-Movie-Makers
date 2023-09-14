d960da410eefe953b39b645f4a22bc2260187bb8
omarjafor <omarjaforchy@gmail.com>
2023-09-14 11:45:39 +0600

    home card

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,27 +9,30 @@ const Home = () => {
         .then(data => setActors(data))
     }, [])
     return (
-        <div className="flex mx-10 w-[800px]">
-            <div className="flex flex-wrap gap-2 w-[600px]">
-                <div className="w-[230px] h-[290px] border border-solid border-gray-300">
-                    <div className="justify-center flex">
-                        <img className="w-[80px] rounded-[50px]" src={actors[1].image} alt="" />
-                    </div>
-                    <h2> {actors[1].name} </h2>
-                    <p>
-                        <small>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
-                        </small>
-                    </p>
-                    <div className="flex justify-around">
-                        <p>salary:{actors[1].salary} $</p>
-                        <p>{actors[1].role}</p>
-                    </div>
-                    <button
-                        className="w-full bg-[rgb(156, 137, 137) p-3 border border-solid border-transparent cursor-pointer]"
-                    >Select</button>
-                </div>
-
+        <div className="mx-10 w-[965px]">
+            <h2 className="text-3xl font-bold text-white">Welcome To Avengers Movie Makers Team</h2><br></br>
+            <div className="flex flex-wrap gap-5 text-white">
+                
+                {
+                    actors.map((actor, idx) => (<div key={idx} className="w-[280px] h-[300px] border border-solid border-blue-500">
+                        <div className="justify-center flex">
+                            <img className="w-[80px] rounded-[50px] pt-2" src={actor.image} alt="" />
+                        </div>
+                        <h2 className="text-2xl font-bold my-3"> {actor.name} </h2>
+                        <p className="text-lg my-2 px-3">
+                            <small>
+                                The Avengers or simply The Avengers, is a 2023 American superhero film.
+                            </small>
+                        </p>
+                        <div className="flex justify-around my-2 font-bold text-lg">
+                            <p>salary: {actor.salary}$</p>
+                            <p>{actor.role}</p>
+                        </div>
+                        <button
+                            className="w-2/3 font-bold rounded-lg bg-blue-400 p-2 mt-1 text-black border border-solid border-transparent cursor-pointer]"
+                        >Select</button>
+                    </div>))
+                }
             </div>
         </div>
     );
