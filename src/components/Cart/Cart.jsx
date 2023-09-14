7f1c31b160c18d587286003dd0b9c7dc516f4979
omarjafor <omarjaforchy@gmail.com>
2023-09-14 12:06:14 +0600

    cart design

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,13 @@
 const Cart = () => {
     return (
-        <div>
-            
+        <div className="text-white">
+            <h1 className="text-2xl font-bold"> Total Budget: 20,000$</h1>
+            <h1 className="text-lg font-bold"> Remaining: {} </h1>
+            <h1 className="text-lg font-bold"> Total Cost: {} </h1>
+            {
+                
+            }
+            <button className="rounded-xl px-4 py-2 font-bold text-lg bg-blue-400" >Complete</button>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 
 const Home = () => {
     const [actors, setActors] = useState([]);
@@ -9,30 +10,35 @@ const Home = () => {
         .then(data => setActors(data))
     }, [])
     return (
-        <div className="mx-10 w-[965px]">
-            <h2 className="text-3xl font-bold text-white">Welcome To Avengers Movie Makers Team</h2><br></br>
-            <div className="flex flex-wrap gap-5 text-white">
-                
-                {
-                    actors.map((actor, idx) => (<div key={idx} className="w-[280px] h-[300px] border border-solid border-blue-500">
-                        <div className="justify-center flex">
-                            <img className="w-[80px] rounded-[50px] pt-2" src={actor.image} alt="" />
-                        </div>
-                        <h2 className="text-2xl font-bold my-3"> {actor.name} </h2>
-                        <p className="text-lg my-2 px-3">
-                            <small>
-                                The Avengers or simply The Avengers, is a 2023 American superhero film.
-                            </small>
-                        </p>
-                        <div className="flex justify-around my-2 font-bold text-lg">
-                            <p>salary: {actor.salary}$</p>
-                            <p>{actor.role}</p>
-                        </div>
-                        <button
-                            className="w-2/3 font-bold rounded-lg bg-blue-400 p-2 mt-1 text-black border border-solid border-transparent cursor-pointer]"
-                        >Select</button>
-                    </div>))
-                }
+        <div className="flex">
+            <div className="mx-10 w-[965px]">
+                <h2 className="text-3xl font-bold text-white">Welcome To Avengers Movie Makers Team</h2><br></br>
+                <div className="flex flex-wrap gap-5 text-white">
+                    {
+                        actors.map((actor, idx) => (<div key={idx} className="w-[280px] h-[300px] border border-solid border-blue-500">
+                            <div className="justify-center flex">
+                                <img className="w-[80px] rounded-[50px] pt-2" src={actor.image} alt="" />
+                            </div>
+                            <h2 className="text-2xl font-bold my-3"> {actor.name} </h2>
+                            <p className="text-lg my-2 px-3">
+                                <small>
+                                    The Avengers or simply The Avengers, is a 2023 American superhero film.
+                                </small>
+                            </p>
+                            <div className="flex justify-around my-2 font-bold text-lg">
+                                <p>salary: {actor.salary}$</p>
+                                <p>{actor.role}</p>
+                            </div>
+                            <button
+                                className="w-2/3 font-bold rounded-lg bg-blue-400 p-2 mt-1 text-black border border-solid border-transparent cursor-pointer]"
+                            >Select</button>
+                        </div>))
+                    }
+                </div>
+            </div>
+            <div className="mt-16 w-1/4 border border-solid border-blue-500">
+                <Cart
+                ></Cart>
             </div>
         </div>
     );
